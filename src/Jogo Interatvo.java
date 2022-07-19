d54d9f413cdcdba47519860fbf3e201e4c0519ef
Matt <matheuspatriotaandrade@gmail.com>
2022-07-19 20:22:30 -0300

    Update do código - projeto parte 2

--- src/Jogo Interatvo.java
+++ src/Jogo Interatvo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Jogo {
     public static void main(String[] args){
         try (Scanner input = new Scanner(System.in)) {
-            System.out.println("Matt, Ross, Tina and Parker heard rummors that a spirit, called ''La Chika'', roams an abandoned school at night and now are going to investigate it further");
+            System.out.println("Matt, Alexander, Tina and Parker heard rummors that a spirit, called ''La Chika'', roams an abandoned school at night and now are going to investigate it further");
             System.out.println("The four went to the school at night, and found a guard at the gate, Tina is too scared and wants to go home, Ross wants to talk with the guard and Parker wants to jump over the gate without him noticing it.");
             System.out.println("-----------------------------------------------------------------------------------------------------------------");
             System.out.println("1 - Do what Tina wants and go to home.");
